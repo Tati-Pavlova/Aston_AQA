b5abcf1c490ea5425f033b2fea1df9fc548de511
Tatiana Pavlova <tativaif7@mail.ru>
2023-12-28 23:49:02 +0300

    Lesson_4

--- lessons/src/main/java/pavlova/tatiana/lessons/MainApp.java
+++ lessons/src/main/java/pavlova/tatiana/lessons/MainApp.java
@@ -1,4 +1,20 @@
 package pavlova.tatiana.lessons;
 
 public class MainApp {
+    public static void main(String[] args)  {
+
+        Person[] persArray = new Person[5];
+        persArray[0] = new Person("Иван Иванов", "Программист", "tati@mail.ru", "+71112223344", 50000, 44);
+        persArray[1] = new Person("Петр Петров", "Аналитик", "tati8@mail.ru", "+71232336676", 70000, 43);
+        persArray[2] = new Person("Виталий Сидоров", "Тестировщик", "tati7@mail.ru", "+71256667890", 80000, 42);
+        persArray[3] = new Person("Максим Прохоров", "Менеджер", "tati6@mail.ru", "+79812345643", 90000, 41);
+        persArray[4] = new Person("Василий Попов", "Тим Лид", "tati5@mail.ru", "+78768884352", 13450, 40);
+
+        persArray[0].print();
+    }
+
 }
+
+
+
+
